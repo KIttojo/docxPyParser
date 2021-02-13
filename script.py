b9e3e055bd998674fd4b6c4a386a497eefdb8edb
Ray <denis_fedorenko_2015@mail.ru>
2021-02-13 22:34:19 +0300

    Now it can parse XML to JSON, res is stored in res.txt file

--- script.py
+++ script.py
@@ -1,11 +1,30 @@
 import os
-import zipfile
+# import zipfile
+import xmltodict, json
+
+import shutil
+import sys
+
+
 
 try:
 	os.rename('test.docx', 'test.zip')
 
-	with zipfile.ZipFile("test.zip", "r") as zip_ref:
-		zip_ref.extractall("targetdir")
+	with open(".\\targetdir\\word\\document.xml", encoding='utf-8') as file:
+		XML_content = file.read()
+		XML_body_indx = XML_content.find("<w:body>")
+		XML_content = XML_content[XML_body_indx:].replace("\n", "").removesuffix("</w:document>")
+		print(XML_content)
+
+		o = xmltodict.parse(XML_content)
+		res = json.dumps(o)
+		print(res)
+
+		res1 = open("res.txt", "w")
+		res1.write(res)
+		res1.close()
+
+
 	print("OK")
 
 except:
